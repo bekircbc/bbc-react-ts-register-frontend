0d4aeea2b6c8c284e2a08a9f3f9daeb0b775fe4d
Bekir - CDI <b.burakcebecii@gmail.com>
2023-01-02 15:20:45 +0100

    initial commit..

---
+++ src/App.tsx
@@ -0,0 +1,100 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.scss';
+import { IUser } from './interfaces';
+import { NavLink, Route, Routes, Navigate } from 'react-router-dom';
+import { PageWelcome } from './pages/PageWelcome';
+import { PageMembers } from './pages/PageMembers';
+import { PageRegister } from './pages/PageRegister';
+import { PageLogin } from './pages/PageLogin';
+import { PageLogout } from './pages/PageLogout';
+import { PageConfirmLink } from './pages/PageConfirmLink';
+import { Page404 } from './pages/Page404';
+
+const baseUrl = import.meta.env.VITE_BACKEND_URL;
+
+function App() {
+	const [currentUser, setCurrentUser] = useState<IUser>({
+		username: '',
+		firstName: '',
+		lastName: '',
+		accessGroups: [],
+	});
+
+	useEffect(() => {
+		(async () => {
+			const data = (
+				await axios.get(`${baseUrl}/current-user`, {
+					withCredentials: true,
+				})
+			).data;
+			const _currentUser = data.currentUser;
+			setCurrentUser(_currentUser);
+		})();
+	}, []);
+
+	return (
+		<div className="App">
+			<h1>Language Tandem Group</h1>
+			{currentUser.username !== 'anonymousUser' && (
+				<div className="userFullName">
+					<span>
+						{currentUser.firstName} {currentUser.lastName}
+					</span>
+				</div>
+			)}
+			<nav>
+				<NavLink to="/welcome">Welcome</NavLink>
+				{currentUser.accessGroups.includes('members') && (
+					<NavLink to="/members">Members</NavLink>
+				)}
+				{currentUser.accessGroups.includes('loggedOutUsers') && (
+					<NavLink to="/register">Register</NavLink>
+				)}
+				{currentUser.accessGroups.includes('loggedOutUsers') && (
+					<NavLink to="/login">Login</NavLink>
+				)}
+				{currentUser.accessGroups.includes('loggedInUsers') && (
+					<NavLink to="/logout">Logout</NavLink>
+				)}
+			</nav>
+
+			<Routes>
+				<Route path="*" element={<Page404 />} />
+				<Route path="/welcome" element={<PageWelcome />} />
+				{currentUser.accessGroups.includes('members') && (
+					<Route path="/members" element={<PageMembers />} />
+				)}
+				{currentUser.accessGroups.includes('loggedOutUsers') && (
+					<Route path="/register" element={<PageRegister />} />
+				)}
+				{currentUser.accessGroups.includes('loggedOutUsers') && (
+					<Route
+						path="/login"
+						element={
+							<PageLogin
+								baseUrl={baseUrl}
+								setCurrentUser={setCurrentUser}
+							/>
+						}
+					/>
+				)}
+				{currentUser.accessGroups.includes('loggedInUsers') && (
+					<Route
+						path="/logout"
+						element={
+							<PageLogout
+								baseUrl={baseUrl}
+								setCurrentUser={setCurrentUser}
+							/>
+						}
+					/>
+				)}
+				<Route path="/confirm-link" element={<PageConfirmLink />} />
+				<Route path="/" element={<Navigate to="/welcome" replace />} />
+			</Routes>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/pages/PageLogin.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { IUser } from '../interfaces';
+import { useNavigate } from 'react-router-dom';
+
+interface IPageLoginProps {
+	baseUrl: string;
+	setCurrentUser: React.Dispatch<React.SetStateAction<IUser>>;
+}
+
+export const PageLogin = (props: IPageLoginProps) => {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+
+	const { baseUrl, setCurrentUser } = props;
+
+	const navigate = useNavigate();
+
+	const handleLoginButton = (e: React.MouseEvent<HTMLElement>) => {
+		e.preventDefault();
+		(async () => {
+			const data = (
+				await axios.post(
+					`${baseUrl}/login`,
+					{ username },
+					{ withCredentials: true }
+				)
+			).data;
+			const _currentUser = data.currentUser;
+			if (_currentUser.username === 'anonymousUser') {
+				console.log('bad login');
+			} else {
+				setCurrentUser(data.currentUser);
+				navigate('/members');
+			}
+		})();
+	};
+
+	return (
+		<form>
+			<fieldset>
+				<div className="row">
+					<label>Username</label>
+					<div>
+						<input
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+							type="text"
+						/>
+					</div>
+				</div>
+				<div className="row">
+					<label>Password</label>
+					<div>
+						<input
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							type="text"
+						/>
+					</div>
+				</div>
+				<div className="buttonRow">
+					<button onClick={handleLoginButton}>Login</button>
+				</div>
+			</fieldset>
+		</form>
+	);
+};
